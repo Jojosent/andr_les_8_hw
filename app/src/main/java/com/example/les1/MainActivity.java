2b3357ae64927e44cb3e921087aa559f820672b1
Jojosent <aldabaizhomart@gmail.com>
2022-04-15 10:30:01 +0600

    les_8_hw

--- app/src/main/java/com/example/les1/MainActivity.java
+++ app/src/main/java/com/example/les1/MainActivity.java
@@ -18,79 +18,116 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    RadioButton rb_excelent, rb_good, rb_nice, rb_poor;
-    CheckBox cb_football, cb_volleyball, cb_tennis, cb_basketball;
-    Button btnJberu;
-    String radioSSS, checkS1,checkS2,checkS3,checkS4;
+    RadioButton rb_s1_jauap1, rb_s1_jauap2,rb_s1_jauap3, rb_s1_jauap4,
+                rb_s2_jauap1, rb_s2_jauap2,rb_s2_jauap3, rb_s2_jauap4,
+                rb_s3_jauap1, rb_s3_jauap2,rb_s3_jauap3, rb_s3_jauap4,
+                rb_s4_jauap1, rb_s4_jauap2,rb_s4_jauap3, rb_s4_jauap4;
+    TextView tv_surak1,tv_surak2,tv_surak3, tv_surak4;
+    Button btn_tekseru;
+    String [] suraktar = {"1+1=?","2+2=?","3+3=?", "4+4=?"};
+    String [][] variant = {
+            {"1","2","3","4"},
+            {"4","3","2","5"},
+            {"8","7","6","4"},
+            {"10","8","9","7"},
+    };
+    String [] jauaptar = {"2","4","6","8"};
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.les7_homewokr);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.les8_homework);
 
-        rb_excelent = findViewById(R.id.rb_excelent);
-        rb_good = findViewById(R.id.rb_good);
-        rb_nice = findViewById(R.id.rb_nice);
-        rb_poor = findViewById(R.id.rb_poor);
+            rb_s1_jauap1 = findViewById(R.id.rb_s1_jauap1);
+            rb_s1_jauap2 = findViewById(R.id.rb_s1_jauap2);
+            rb_s1_jauap3 = findViewById(R.id.rb_s1_jauap3);
+            rb_s1_jauap4 = findViewById(R.id.rb_s1_jauap4);
 
-        cb_football = findViewById(R.id.cb_football);
-        cb_volleyball = findViewById(R.id.cb_volleyball);
-        cb_tennis = findViewById(R.id.cb_tennis);
-        cb_basketball = findViewById(R.id.cb_basketball);
+            rb_s2_jauap1 = findViewById(R.id.rb_s2_jauap1);
+            rb_s2_jauap2 = findViewById(R.id.rb_s2_jauap2);
+            rb_s2_jauap3 = findViewById(R.id.rb_s2_jauap3);
+            rb_s2_jauap4 = findViewById(R.id.rb_s2_jauap4);
 
-        btnJberu = findViewById(R.id.btnJberu);
+            rb_s3_jauap1 = findViewById(R.id.rb_s3_jauap1);
+            rb_s3_jauap2 = findViewById(R.id.rb_s3_jauap2);
+            rb_s3_jauap3 = findViewById(R.id.rb_s3_jauap3);
+            rb_s3_jauap4 = findViewById(R.id.rb_s3_jauap4);
 
-        btnJberu.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                if(rb_excelent.isChecked()){
-                    radioSSS = "Selected Radio Button: " + rb_excelent.getText();
-                }else if(rb_good.isChecked()){
-                    radioSSS = "Selected Radio Button: " + rb_good.getText();
-                }else if(rb_nice.isChecked()){
-                    radioSSS = "Selected Radio Button: " + rb_nice.getText();
-                }else if(rb_poor.isChecked()){
-                    radioSSS = "Selected Radio Button: " + rb_poor.getText();
+            rb_s4_jauap1 = findViewById(R.id.rb_s4_jauap1);
+            rb_s4_jauap2 = findViewById(R.id.rb_s4_jauap2);
+            rb_s4_jauap3 = findViewById(R.id.rb_s4_jauap3);
+            rb_s4_jauap4 = findViewById(R.id.rb_s4_jauap4);
+
+            tv_surak1 = findViewById(R.id.tv_surak1);
+            tv_surak2 = findViewById(R.id.tv_surak2);
+            tv_surak3 = findViewById(R.id.tv_surak3);
+            tv_surak4 = findViewById(R.id.tv_surak4);
+
+            btn_tekseru = findViewById(R.id.btn_tekseru);
+            suraktarJinau();
+
+            btn_tekseru.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tekseris(tv_surak1,rb_s1_jauap1,rb_s1_jauap2,rb_s1_jauap3,rb_s1_jauap4,0);
+                    tekseris(tv_surak2,rb_s2_jauap1,rb_s2_jauap2,rb_s2_jauap3,rb_s2_jauap4,1);
+                    tekseris(tv_surak3,rb_s3_jauap1,rb_s3_jauap2,rb_s3_jauap3,rb_s3_jauap4,2);
+                    tekseris(tv_surak4,rb_s4_jauap1,rb_s4_jauap2,rb_s4_jauap3,rb_s4_jauap4,3);
                 }
+            });
 
-
-                if(cb_football.isChecked()){
-                    checkS1 = cb_football.getText() + ": Yes";
-                }else{
-                    checkS1 = cb_football.getText() +":No";
-                }
-
-
-                if(cb_volleyball.isChecked()){
-                    checkS2 = cb_volleyball.getText() +":Yes";
-                }else{
-                    checkS2 = cb_volleyball.getText() +":No";
-                }
-
-
-                if(cb_tennis.isChecked()){
-                    checkS3 = cb_tennis.getText() +":Yes";
-                }else{
-                    checkS3 = cb_tennis.getText() +":No";
-                }
-
-
-                if(cb_basketball.isChecked()){
-                    checkS4 = cb_basketball.getText() +":Yes";
-                }else{
-                    checkS4 = cb_basketball.getText() +":No";
-                }
-
-
-                String result = radioSSS + "\n"+
-                        "Checkbox Choices:" + "\n"+
-                        checkS1 + "\n"+
-                        checkS2+"\n"+
-                        checkS3+"\n"+
-                        checkS4+"\n";
-                Toast.makeText(MainActivity.this,result,Toast.LENGTH_LONG).show();
+        }
+        public void tekseris(TextView tv_surak, RadioButton rb1, RadioButton rb2,RadioButton rb3, RadioButton rb4, int index){
+            if(rb1.isChecked()&&rb1.getText().toString().equals(jauaptar[index])){
+                tv_surak.setText(suraktar[index]+"\nDurys");
+                tv_surak.setTextColor(getResources().getColor(R.color.green));
             }
-        });
+            else if(rb2.isChecked()&&rb2.getText().toString().equals(jauaptar[index])){
+                tv_surak.setText(suraktar[index]+"\nDurys");
+                tv_surak.setTextColor(getResources().getColor(R.color.green));
+            }
+            else if(rb3.isChecked()&&rb3.getText().toString().equals(jauaptar[index])){
+                tv_surak.setText(suraktar[index]+"\nDurys");
+                tv_surak.setTextColor(getResources().getColor(R.color.green));
+            }
+            else if(rb4.isChecked()&&rb4.getText().toString().equals(jauaptar[index])){
+                tv_surak.setText(suraktar[index]+"\nDurys");
+                tv_surak.setTextColor(getResources().getColor(R.color.green));
+            }
+            else{
+                tv_surak.setText(suraktar[index]+"\nKate");
+                tv_surak.setTextColor(getResources().getColor(R.color.red));
+
+            }
+        }
+
+
+        public void suraktarJinau(){
+            tv_surak1.setText(suraktar[0]);
+            rb_s1_jauap1.setText(variant[0][0]);
+            rb_s1_jauap2.setText(variant[0][1]);
+            rb_s1_jauap3.setText(variant[0][2]);
+            rb_s1_jauap4.setText(variant[0][3]);
+
+            tv_surak2.setText(suraktar[1]);
+            rb_s2_jauap1.setText(variant[1][0]);
+            rb_s2_jauap2.setText(variant[1][1]);
+            rb_s2_jauap3.setText(variant[1][2]);
+            rb_s2_jauap4.setText(variant[1][3]);
+
+            tv_surak3.setText(suraktar[2]);
+            rb_s3_jauap1.setText(variant[2][0]);
+            rb_s3_jauap2.setText(variant[2][1]);
+            rb_s3_jauap3.setText(variant[2][2]);
+            rb_s3_jauap4.setText(variant[2][3]);
+
+            tv_surak4.setText(suraktar[3]);
+            rb_s4_jauap1.setText(variant[3][0]);
+            rb_s4_jauap2.setText(variant[3][1]);
+            rb_s4_jauap3.setText(variant[3][2]);
+            rb_s4_jauap4.setText(variant[3][3]);
+
+
+        }
     }
-}
